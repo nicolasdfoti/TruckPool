cec96284f012944be7d60119a979c31d98b811b7
Nicolás Foti <nicolasdfoti@gmail.com>
2026-02-17 18:19:35 -0300

    Updated Forms CSS | Updated User class | Updated About Us page

--- Models/User.cs
+++ Models/User.cs
@@ -4,5 +4,9 @@ public class User
 {
     public Guid UserId { get; set; }
     public string Name { get; set; } = "";
+    public string Surname { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Phone { get; set; } = "";
     public string Role { get; set; } = "";
+    public string Password { get; set; } = "";
 }
